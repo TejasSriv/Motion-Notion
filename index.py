e8c8d6b7a1946cc151d87785a11eec8b17955756
TejasSriv <tejas.sriv.44@gmail.com>
2024-11-24 12:35:03 +0530

    Added functionality to reset first frame if the camera is moved

--- index.py
+++ index.py
@@ -6,15 +6,20 @@ _, first_frame = cap.read()
 first_frame = cv2.cvtColor(first_frame, cv2.COLOR_BGR2GRAY)
 first_frame = cv2.GaussianBlur(first_frame, (21, 21), 0)
 
+print("Motion Notion started.\n")
+
 while True:
-    _, frame = cap.read()
+    ret, frame = cap.read()
+    if not ret:
+        print("Failed to grab frame.")
+        break
+    
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     gray_frame = cv2.GaussianBlur(gray_frame, (21, 21), 0)
     
     frame_diff = cv2.absdiff(first_frame, gray_frame)
     
     _, thresh_frame = cv2.threshold(frame_diff, 25, 255, cv2.THRESH_BINARY)
-    thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
     thresh_frame = cv2.dilate(thresh_frame, None, iterations=2)
     
     contours, _ = cv2.findContours(thresh_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
@@ -28,9 +33,14 @@ while True:
         
     cv2.imshow("Motion Detector", frame)
     
-    key = cv2.waitKey(1)
+    key = cv2.waitKey(1) & 0xFF
+    
     if key == ord('q'):
         break
+    
+    if key == ord('r'):
+        first_frame = gray_frame
+        print("First frame reset!")
 
 cap.release()
 cv2.destroyAllWindows()
